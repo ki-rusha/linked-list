902384c25ef99d5fb5a09b6692b8a357514374f3
Lyubov Kiprushova <kiprushova.ljubov@gmail.com>
2019-10-26 00:15:15 +0300

    br2: add tests for improved deleteFirst method

--- tests/base.test.js
+++ tests/base.test.js
@@ -8,6 +8,10 @@ console.log(ll);
 
 ll.deleteFirst()
 ll.addFirst(3);
+ll.addFirst(4);
 console.log(ll);
-ll.deleteFirst();
-console.log(ll);
+let first = ll.deleteFirst();
+console.log(ll, first);
+first = ll.deleteFirst();
+console.log(ll, first);
+console.log(ll, ll.deleteFirst())
