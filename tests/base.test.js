e6fcd7c04de87c51cb498b4e63e41f3c0c0d146b
Lyubov Kiprushova <kiprushova.ljubov@gmail.com>
2019-10-26 00:03:07 +0300

    hotfix1: add tests for deleteFirst method

--- tests/base.test.js
+++ tests/base.test.js
@@ -11,3 +11,21 @@ ll.addFirst(3);
 console.log(ll);
 ll.deleteFirst();
 console.log(ll);
+ll.deleteFirst();
+ll.deleteFirst();
+console.log(ll);
+
+const l1 = new LinkedList();
+let i = 10;
+while (++i < 100) {
+    l1.addFirst(i);
+}
+
+console.log(l1);
+
+let j = 0;
+while (++j < 100) {
+    l1.deleteFirst();
+}
+
+console.log(l1);
